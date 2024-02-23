490cacab919eae65cac9b3c59891e863f35658ff
pallav chavda <pallavdrj7533@gmail.com>
2024-02-23 16:50:43 +0530

    RandomForestClassifier Project

--- Corona Dash/app.py
+++ Corona Dash/app.py
@@ -1,2 +1,20 @@
-import numpy as np
-import pandas as pd
+import streamlit as st
+
+st.title('Sample App')
+
+st.image("unnamed.jpg")
+
+st.header("CampusX")
+st.text("Your home for Data Science")
+
+st.subheader("CampusX is an online platform for learning data science")
+
+st.markdown("""
+Programs provided by CampusX
+
+- Data Science Mentorship Program
+- Data Analytics Mentorship Program
+- Data Engineering Mentorship Program
+- MLOPs
+- SQL Fundamentals
+""")
